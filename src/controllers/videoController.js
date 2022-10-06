b53284da32c2d87f8c3b4939e8b1e0e5907442c8
TedHa <tedha012@gmail.com>
2022-10-06 22:48:56 +0900

    settingPug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,16 +1,10 @@
-export const trending = (req, res) => res.send("Home Page Videos");
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 
-export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
-}
-    
-export const edit = (req, res) => {
-    console.log(req.params);
-    res.send("Edit Video")
-};
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
 export const deleteVideo = (req, res) => {
-    console.log(req.params);
-    res.send("Delete Video");
+  console.log(req.params);
+  res.send("Delete Video");
 };
--- src/server.js
+++ src/server.js
@@ -11,12 +11,14 @@ const app = express();
 const logger = morgan("dev");
 app.use(logger);
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 const handleListening = () => {
-    console.log(`Server listenting on port http:localhost:${PORT}`);
-}
+  console.log(`Server listenting on port http:localhost:${PORT}`);
+};
 
 app.listen(PORT, handleListening);
