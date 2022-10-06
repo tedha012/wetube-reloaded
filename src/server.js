9a8ebf1a3e19a0bfba532b7dce985cc7a99e1991
tedha012 <tedha012@gmail.com>
2022-10-06 13:54:34 +0900

    middleWare

--- src/server.js
+++ src/server.js
@@ -4,8 +4,16 @@ const PORT =4000;
 
 const app = express();
 
-const goddipMiddleware = (req, res, next) => {
-    console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>");
+    }
+    console.log("Allowed, you may continue")
     next();
 }
 
@@ -13,7 +21,15 @@ const handleHome = (req, res) => {
     return res.end("I love middleware");
 }
 
-app.get("/", goddipMiddleware, handleHome);
+const handleProtected = (req, res) => {
+    return res.send("Welcome to  the private lounge");
+}
+
+app.use(logger);
+app.use(privateMiddleware);
+
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
 
 const handleListening = () => {
     console.log(`Server listenting on port http:localhost:${PORT}`);
