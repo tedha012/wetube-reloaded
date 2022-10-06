7c3df572f8f5cae4c9872e05d9a092d8787dcebd
tedha012 <tedha012@gmail.com>
2022-10-06 18:44:44 +0900

    makeControllers

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
+import { edit, remove } from "../controllers/userController"
 
 const userRouter = express.Router();
 
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
+userRouter.get("/edit", edit);
+userRouter.get("/remove", remove);
 
 
 export default userRouter;
