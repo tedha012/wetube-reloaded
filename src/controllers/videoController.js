4defae35452976843b52d62bcd36bf783400033e
tedha012 <tedha012@gmail.com>
2022-10-07 11:51:21 +0900

    PugIteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,9 @@
-const fakeUser = {
-  username:"Ted",
-  loggedIn: false,
-}
-
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
-export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
-export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
+export const trending = (req, res) => {
+  const videos = [];
+  return res.render("home", { pageTitle: "Home", videos })
+};
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
