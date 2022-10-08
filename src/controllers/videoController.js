f2a6415bf0302f0ad077b4b814fb27d588b6a9aa
TedHa <tedha012@gmail.com>
2022-10-08 16:17:57 +0900

    Creating Videos in DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -18,9 +18,19 @@ export const postEdit = (req, res) => {
   return res.redirect(`/videos/${id}`);
 };
 export const getUpload = (req, res) => {
-  return res.render("upload", { pageTitle: `Upload Video` });
+  return res.render("upload", { pageTitle: "Upload Video" });
 };
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  return res.redirect(`/`);
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  await Video.create({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  return res.redirect("/");
 };
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController"
+import { edit, remove, logout, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
@@ -7,6 +7,5 @@ userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
 userRouter.get(":id", see);
-
 
 export default userRouter;
