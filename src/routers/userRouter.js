2fa7d311901af0d60aa7f3e4c795e59413acb66c
tedha012 <tedha012@gmail.com>
2022-10-11 10:37:50 +0900

    Edit Profile GET

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -7,12 +7,14 @@ import {
   finishGithubLogin,
   startKakaoLogin,
   finishKakaoLogin,
+  getEdit,
+  postEdit,
 } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/kakao/start", startKakaoLogin);
