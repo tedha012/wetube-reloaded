b5386e79dea02f12f8033c8ffb5cecca47dd6d10
tedha012 <tedha012@gmail.com>
2022-10-13 11:53:18 +0900

    videoPlayer : add play and pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,24 @@
-console.log("hello");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  // if the video is playing, pause it
+  if (video.paused) {
+    video.play();
+  } else {
+    //else play the video
+    video.pause();
+  }
+};
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
+muteBtn.addEventListener("click", handleMute);
