e3c76741bb206aee3d41510a21b56263ef6d2555
tedha012 <tedha012@gmail.com>
2022-10-14 19:58:24 +0900

    Recording Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,42 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+let stream;
+let recorder;
+
+const handleDownload = () => {};
+
+const hadleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", hadleStop);
+  startBtn.addEventListener("click", handleStart);
+
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", hadleStop);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    const videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
     video: true,
   });
   video.srcObject = stream;
   video.play();
 };
+init();
 
 startBtn.addEventListener("click", handleStart);
