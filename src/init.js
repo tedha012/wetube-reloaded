ffa7288e167a3df01b2e6bb93a0f695f09c016b9
tedha012 <tedha012@gmail.com>
2022-10-17 15:48:39 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = proess.env.PORT || 4000;
 
 const handleListening = () => {
   console.log(`✔ Server listenting on http:localhost:${PORT}`);
